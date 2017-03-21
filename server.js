2fedde79ffd59254216e2c51ee94ec6e5a3ade6c
Hamza Ghani <Hamza@wireless-10-146-43-131.public.utexas.edu>
2017-03-20 22:56:42 -0500

    added routes for api

--- server.js
+++ server.js
@@ -28,6 +28,77 @@ app.use(function(req, res, next) {
 // log all requests to the console
 app.use(morgan('dev'));
 
+
+app.get('/', function (req, res) {
+    res.send('This is Polar\'s Home Page');
+
+});
+
+var polarApirRouter = express.Router();
+
+polarApirRouter.route('/users')
+
+    .post(function(req, res) {
+         
+
+    });
+
+
+polarApirRouter.route('/users/:userid')
+
+    .get(function(req, res) {
+
+
+
+    })
+
+    .put(function(req, res) {
+
+
+    })
+
+    .delete(function(req, res) {
+
+
+    });
+
+
+polarApirRouter.route('/users/:userid/notes') 
+    
+    .get(function(req, res) {
+
+
+    })
+
+    .post(function(req, res) {
+
+
+    });
+
+
+polarApirRouter.route('/users/:userid/notes/:noteid') 
+
+    .get(function(req, res) {
+
+
+    })
+
+    .post(function(req, res) {
+
+
+    })
+
+
+    .delete(function(req, res) {
+
+
+    });
+
+
+
+
+
+
 // set static files location
 // used for requests that our frontend will make
 app.use(express.static(__dirname + '/public'));
