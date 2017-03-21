9f04f47d86946234997ad7c1ce4afd1c85f7082a
Hamza Ghani <Hamza@wireless-10-146-43-131.public.utexas.edu>
2017-03-20 23:34:05 -0500

    added implementation for user requests

--- app/routes/api.js
+++ app/routes/api.js
@@ -16,25 +16,62 @@ module.exports = function(app, express) {
         user.password = req.body.password;
         user.notes = null;
 
-        res.json({ message: 'User created!' });
+        user.save(function(err) {
+            if (err) {
+                //duplicate entry
+                if (err.code == 11000) 
+                    return res.json({ success: false, message: 'A user with that username already exists.'});
+                else 
+                    return res.send(err); 
+                }
+
+                //return a message
+                res.json({ message: 'User created!'}); 
+        });          
+
     });
 
 
   apiRouter.route('/users/:user_id')
 
     .get(function(req, res) {
-
-
+        User.findById(req.params.user_id, function(err, user) {
+                    if (err) return res.send(err);
+                    // return that user
+                    res.json(user);
+        });
 
     })
 
     .put(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+        
+        if (err) return res.send(err); 
 
+        //set the new user information if it exists in the request
+        if (req.body.name) user.name = req.body.name;
+        if (req.body.username) user.username = req.body.username;
+        if (req.body.password) user.password = req.body.password;
+        
+        //save the user
+        user.save(function(err) {
+            if (err) return res.send(err); 
+
+            //return a message
+            res.json({ message: 'User updated!' });
+        });
+     })
 
     })
 
     .delete(function(req, res) {
+        User.remove({
+            _id: req.params.user_id
+            }, function(err, user) {
+                if (err) return res.send(err); 
 
+                res.json({ message: 'Successfully deleted' });
+        )}
 
     });
 
