1b44fd6ddfd38644c9f0db0e33160eaf4b2e23ee
Max Wang <maxwang051@gmail.com>
2017-04-19 17:07:56 -0500

    added config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'port': 8080,
+  'database': 'mongodb://polar:pw456@ds137100.mlab.com:37100/polar',
+  'secret': 'mynameismax',
+
+  'googleAuth': {
+    'clientId' : '739786731369-6bsi41obshkjhu2r4p9cr455mjfdr75r.apps.googleusercontent.com',
+    'clientSecret' : 'YPJpKTTwt96xgMdaAopiSBLg',
+    'callbackURL' : 'http://localhost:8080/auth/google/callback'
+  }
+};
